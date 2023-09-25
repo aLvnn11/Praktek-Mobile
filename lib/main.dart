850074a91903511fc8b8b2400121881e074fd04c
COLD <129246283+aLvnn11@users.noreply.github.com>
2023-09-25 14:00:05 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,99 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:basic/provider.dart';
-import 'package:basic/screen11.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_spinbox/flutter_spinbox.dart';
 
 void main() {
-  runApp(MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => Provider12())],
-      child: const MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Stream Spin Box',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      debugShowCheckedModeBanner: false,
-      home: const Screen12(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final StreamController<double> _streamController = StreamController<double>();
+  double _score = 0.0;
+
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Stream Spin Box'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              StreamBuilder<double>(
+                stream: _streamController.stream,
+                builder: (context, snapshot) {
+                  final value = snapshot.data ?? 0.0;
+                  return Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(color: Colors.red[200]),
+                    child: value == 10.0
+                        ? Image.asset(
+                            'assets/your_image.png', // Ganti dengan path gambar Anda
+                            width: 200.0,
+                            height: 200.0,
+                            fit: BoxFit.fitHeight,
+                          )
+                        : Center(
+                            child: Text(
+                              '$_score',
+                              style: TextStyle(fontSize: 24.0),
+                            ),
+                          ),
+                  );
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SpinBox(
+                  max: 10.0,
+                  min: 0.0,
+                  value: _score,
+                  decimals: 1,
+                  step: 0.1,
+                  decoration: InputDecoration(labelText: 'Decimal'),
+                  onChanged: (value) {
+                    _streamController.sink.add(value);
+                    setState(() {
+                      _score = value;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
