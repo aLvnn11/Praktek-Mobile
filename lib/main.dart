202f2789d3661d1914b08ece09330e70a6a12cde
COLD <129246283+aLvnn11@users.noreply.github.com>
2023-10-02 14:39:07 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:basic/components/MyProvider.dart';
-import 'package:basic/components/db.helper.dart';
 import 'package:flutter/material.dart';
 import 'package:basic/components/ShoppingList.dart';
+import 'package:basic/components/ShoppingListDialog.dart';
+import 'package:basic/components/db.helper.dart';
 import 'package:provider/provider.dart';
 import 'package:basic/components/ItemScreen.dart';
-
-import 'package:basic/components/ShoppingListDialog.dart';
+import 'package:basic/components/MyProvider.dart';
 
 class Screen extends StatefulWidget {
   const Screen({Key? key}) : super(key: key);
@@ -16,7 +15,6 @@ class Screen extends StatefulWidget {
 
 class _ScreenState extends State<Screen> {
   int id = 0;
-
   DBHelper _dbHelper = DBHelper();
 
   @override
@@ -37,68 +35,69 @@ class _ScreenState extends State<Screen> {
         ],
       ),
       body: ListView.builder(
-          itemCount:
-              tmp.getShoppingList != null ? tmp.getShoppingList.length : 0,
-          itemBuilder: (BuildContext context, int index) {
-            return Dismissible(
-                key: Key(tmp.getShoppingList[index].id.toString()),
-                onDismissed: (direction) {
-                  String tmpName = tmp.getShoppingList[index].name;
-                  int tmpId = tmp.getShoppingList[index].id;
-                  setState(() {
-                    tmp.deletedById(tmp.getShoppingList[index]);
-                  });
-                  _dbHelper.deletedShoppingList(tmpId);
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    content: Text('$tmpName deleted'),
-                  ));
-                },
-                child: ListTile(
-                  title: Text(tmp.getShoppingList[index].name),
-                  leading: Column(
-                    children: [
-                      CircleAvatar(
-                        child: Text("${tmp.getShoppingList[index].sum}"),
-                      ),
-                      CircleAvatar(
-                        child: Text("${tmp.getShoppingList[index].price}"),
-                      ),
-                    ],
+        itemCount: tmp.getShoppingList != null ? tmp.getShoppingList.length : 0,
+        itemBuilder: (BuildContext context, int index) {
+          return Dismissible(
+            key: Key(tmp.getShoppingList[index].id.toString()),
+            onDismissed: (direction) {
+              String tmpName = tmp.getShoppingList[index].name;
+              int tmpId = tmp.getShoppingList[index].id;
+              setState(() {
+                tmp.deletedById(tmp.getShoppingList[index]);
+              });
+              _dbHelper.deletedShoppingList(tmpId);
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('$tmpName deleted'),
+              ));
+            },
+            child: ListTile(
+              title: Text(tmp.getShoppingList[index].name),
+              leading: Column(
+                children: [
+                  CircleAvatar(
+                    child: Text("${tmp.getShoppingList[index].sum}"),
                   ),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return ItemsScreen(tmp.getShoppingList[index]);
-                    }));
-                  },
-                  trailing: IconButton(
-                      icon: Icon(Icons.edit),
-                      onPressed: () {
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              return ShoppingListDialog(_dbHelper).buildDialog(
-                                  context, tmp.getShoppingList[index], false);
-                            });
-                        _dbHelper
-                            .getmyShoppingList()
-                            .then((value) => tmp.setShoppingList = value);
-                      }),
-                ));
-          }),
+                  CircleAvatar(
+                    child: Text("${tmp.getShoppingList[index].price}"),
+                  ),
+                ],
+              ),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ItemsScreen(tmp.getShoppingList[index]);
+                }));
+              },
+              trailing: IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return ShoppingListDialog(_dbHelper).buildDialog(
+                        context, tmp.getShoppingList[index], false);
+                    },
+                  );
+                  _dbHelper.getmyShoppingList().then((value) => tmp.setShoppingList = value);
+                },
+              ),
+            ),
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            await showDialog(
-                context: context,
-                builder: (context) {
-                  return ShoppingListDialog(_dbHelper)
-                      .buildDialog(context, ShoppingList(++id, "", 0, 0), true);
-                });
-            _dbHelper
-                .getmyShoppingList()
-                .then((value) => tmp.setShoppingList = value);
-          },
-          child: Icon(Icons.add)),
+        onPressed: () async {
+          await showDialog(
+            context: context,
+            builder: (context) {
+              // Tambahkan input field untuk price
+              return ShoppingListDialog(_dbHelper).buildDialog(
+                context, ShoppingList(++id, "", 0, 0), true);
+            },
+          );
+          _dbHelper.getmyShoppingList().then((value) => tmp.setShoppingList = value);
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 
